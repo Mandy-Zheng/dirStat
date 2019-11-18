1c3d1d2b7c1f95c5c8da7641d483cdf3d5a31c03
Amanda Zheng <azheng00@stuy.edu>
2019-11-17 23:19:56 -0500

    finished assignment

--- main.c
+++ main.c
@@ -5,21 +5,28 @@
 #include <sys/types.h>
 #include <string.h>
 #include <dirent.h>
+#include <string.h>
+#include <math.h>
 int main(int argc, char const *argv[]) {
   DIR * opened=opendir(".");
   int size=0;
+  char directories[1000]="Directories:";
+  char files[1000]="Regular Files:";
   if(opened!=NULL){
     struct dirent * read=readdir(opened);
-    printf("Statistics for Current Directory\n\n");
+    printf("Statistics for Current Directory:\n");
     while(read != NULL){
       struct stat readfile;
-      if(stat(read->d_name, &readfile)==-1){
+      int items=stat(read->d_name, &readfile);
+      if(items==-1){
         printf("Error: %s\n",strerror(errno));
       }else{
-        if(read->d_type == DT_DIR){
-          printf("Directory %s \n",read->d_name/*,read->d_type*/);
+        if((readfile.st_mode & S_IFMT) == S_IFDIR){
+          strcat(directories,"\n\t");
+          strcat(directories,read->d_name);
         }else{
-          printf("File %s \n",read->d_name/*,read->d_type*/);
+          strcat(files,"\n\t");
+          strcat(files,read->d_name);
         }
       }
       size+=readfile.st_size;
@@ -28,7 +35,22 @@ int main(int argc, char const *argv[]) {
   }else{
     printf("Error %s\n",strerror(errno));
   }
-
-  printf("\n Current Directory Size: %d Bytes\n",size );
+  if(size>=1000000000){
+    double newsize=size/1000000000.0;
+    printf("Total Current Directory Size: %.2f GB\n",newsize);
+  }
+  else if(size>=1000000){
+    double newsize=size/1000000.0;
+    printf("Total Current Directory Size: %.2f MB\n",newsize);
+  }
+  else if(size>=1000){
+    float newsize=size/1000.0;
+    printf("Total Current Directory Size: %.2f KB\n",newsize);
+  }
+  else{
+    printf("Total Current Directory Size: %d Bytes\n",size);
+  }
+  printf("%s\n\n",directories);
+  printf("%s\n",files);
   return 0;
 }
